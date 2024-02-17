971fa0f841922456aa18f39b7daa42b9cf6dc0a4
Amdadul Haque <77147665+itz-AmdadulHaque@users.noreply.github.com>
2024-02-17 12:49:03 +0600

    Update asyncHandler.js comments

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,4 @@
-// handling asycn operation
+// handling asycn operation and error, now we don't need to use try...catch 
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
